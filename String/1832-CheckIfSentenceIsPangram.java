62b7087aa851a9c666fc248d507cf17df9a364b9
Vivek Prasad <vivekprasad345@gmail.com>
2021-04-18 15:32:27 +0530

    Create 1832-CheckIfSentenceIsPangram.java

---
+++ String/1832-CheckIfSentenceIsPangram.java
@@ -0,0 +1,27 @@
+/*
+https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+*/
+
+class Solution {
+    public boolean checkIfPangram(String s) {
+        
+        int a[] =new int[26];
+        
+        int l = s.length();
+        int c =0;
+        for(int i=0;i<l;i++){
+            
+            char ch = s.charAt(i);
+            int p = (int)ch;
+            p = p%97;
+            if(a[p] == 0){
+                a[p] = l;
+                c++;
+            }
+        }
+        if(c==26)
+            return true;
+        else
+            return false;
+    }
+}
