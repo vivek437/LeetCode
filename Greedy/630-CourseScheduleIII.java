be99c9d2dd0114ea2aa85ad5eb80c093da1061a8
Vivek Prasad <vivekprasad345@gmail.com>
2021-05-02 20:57:35 +0530

    Create 630-CourseScheduleIII.java

---
+++ Greedy/630-CourseScheduleIII.java
@@ -0,0 +1,26 @@
+
+
+class Solution{
+  
+  
+    public int scheduleCourse(int[][] courses) {
+      
+      Arrays.sort(courses, (a,b) -> a[1] - b[1]);
+      
+      PriorityQueue<Integer> pq =  new PriorityQueue < > ((a, b) -> b - a);
+      int time = 0;
+      for(int[] c : courses){
+        
+          if(time + c[1] >  c[0])
+          {
+            time = time + c[1];
+            pq.offer(c[0]);
+          }
+          else if( !pq.empty() && pq.peek()>c[0]){
+                time += c[0] - queue.poll();
+                queue.offer(c[0]);
+          }
+       }
+        return pq.size();
+    }
+}
