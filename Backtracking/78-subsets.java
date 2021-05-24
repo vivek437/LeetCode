202ab95896403af3ed514fbe1e19dcb0de2b3c0e
Vivek Prasad <vivekprasad345@gmail.com>
2021-05-25 01:07:45 +0530

    78-Subsets

---
+++ Backtracking/78-subsets.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    int N;
+    List<List<Integer>> ans;
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        N = nums.length;
+        ans = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        boolean[] used = new boolean[N];
+        List<Integer> ar = new ArrayList<Integer>();
+        
+        solve(nums,ar,used,0);
+        return ans;
+    }
+   
+    void solve(int[] nums, List<Integer> ls,boolean[] used,int start) {
+        
+          
+         ans.add(new ArrayList<Integer>(ls));
+
+      
+        for(int i=start;i<N;i++){   
+                if( used[i])
+                    continue;
+                
+                used[i] = true;
+                ls.add(nums[i]);
+                solve(nums,ls,used,i+1);
+                ls.remove(ls.size()-1); 
+                used[i] = false;
+              
+        }
+    }
+}
