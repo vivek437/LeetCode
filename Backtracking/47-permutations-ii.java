d1c3bc201588ce1a6944c2a1c70e2ea93166ddcb
Vivek Prasad <vivekprasad345@gmail.com>
2021-05-25 00:57:15 +0530

    47-Permutations II

---
+++ Backtracking/47-permutations-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    int N;
+    List<List<Integer>> ans;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        
+        N = nums.length;
+        ans = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        boolean[] used = new boolean[N];
+        List<Integer> ar = new ArrayList<Integer>();
+        
+        solve(nums,ar,used);
+        return ans;
+    }
+   
+    void solve(int[] nums, List<Integer> ls,boolean[] used) {
+        
+        if( ls.size() == N) {           
+             ans.add(new ArrayList<Integer>(ls));
+             return;
+        }
+      
+        for(int i=0;i<N;i++){   
+                if( used[i])
+                    continue;
+                
+                used[i] = true;
+                ls.add(nums[i]);
+                solve(nums,ls,used);
+                ls.remove(ls.size()-1); 
+                used[i] = false;
+                
+                while(i+1<nums.length && nums[i] == nums[i+1])
+                    ++i;
+               
+        }
+    }
+}
